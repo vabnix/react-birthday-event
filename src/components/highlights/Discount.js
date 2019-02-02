8f09794a83c3ac520e3eda40dcd14f5fbf1e1fda
LoneWolfAccount <lonewolfaccount@lw-macbook-pro.local>
2019-02-01 21:30:09 -0600

    adding more component

--- src/components/highlights/Discount.js
+++ src/components/highlights/Discount.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Fade from 'react-reveal/Fade';
 import Slide from 'react-reveal/Slide';
 
- const Discount = () => {
+ class Discount extends Component {
+    state = {
+        discountStart:0,
+        discountEnd:30
+    }
+
+    percentage(){
+      if(this.state.discountStart < this.state.discountEnd){
+        this.setState({
+          discountStart: this.state.discountStart + 1
+        })
+      }
+    }
+
+    componentDidUpdate(){
+      setTimeout(()=>{
+        this.percentage()
+      },30)
+    }
+
+   render(){
     return (
       <div className="center_wrapper">
         <div className="discount_wrapper">
-           <Fade>
+           <Fade
+             onReveal={()=> this.percentage()}
+           >
            <div className="discount_percentage">
-           <span>30%</span>
+           <span>{this.state.discountStart}%</span>
            <span>OFF</span>
            </div>
            </Fade>
@@ -16,11 +38,14 @@ import Slide from 'react-reveal/Slide';
            <div className="discount_description">
             <h3>Purchase ticket before 20th FEB</h3>
             <p>Disney World tickets are available discounted year-round <br/>First Time <strong>Texas Resident</strong> Platinum Pass: $797.69 for all ages (3+), including tax ($154.43 off the regular price)</p>
+            <div>Button</div>
            </div>
+
            </Slide>
         </div>
       </div>
     )
+   }
 }
 
 export default Discount;
